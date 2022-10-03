01bd0dbf8263e37f2b9be0118dae742501989bab
Akash Grover <akash.grover@ditstek.com>
2022-10-03 12:44:59 +0100

    jenkins setting

--- testing/src/main/java/com/testing/TestingApplication.java
+++ testing/src/main/java/com/testing/TestingApplication.java
@@ -2,12 +2,18 @@ package com.testing;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class TestingApplication {
+public class TestingApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application){
+		return application.sources(TestingApplication.class);
+	}
 
 	public static void main(String[] args) {
-
 		SpringApplication.run(TestingApplication.class, args);
 	}
 
